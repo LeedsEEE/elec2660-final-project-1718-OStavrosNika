dc5f16a6a256ed12ac59e0a595606348de154fc5
William Mooney [el16wtm] <el16wtm@eentpc199.leeds.ac.uk>
2017-12-07 13:56:48 +0000

    BMR code semi complete

--- Stength App/BMR.m
+++ Stength App/BMR.m
@@ -11,5 +11,35 @@
 @implementation BMR
 
 - (IBAction)calculateAction:(id)sender {
+    
+#pragma mark Button Pressed
+    
+    - (IBAction)calculateAction:(id)sender {
+        
+    
+        NSLog(@"Calculate Pressed"); // gives time when calculate was pressed
+        
+        float weight = [[self.weightTextField text] floatValue];
+        float height = [[self.heightTextField text] floatValue];
+        float age = [[self.ageTextField text] floatValue];
+        float gender = [[self.genderTextField text] floatValue];
+        
+        NSLog(@"weight: %f height: %f age: %f", weight, height, age); //prints weight and height and age
+        
+        if gender == m;
+        {
+        // equation for BMR
+        float result = 66.47 + (13.7*weight)+(5*height)-(6.8*age)
+        }
+        else {
+            float result = 655.1 + (9.6*weight) + (1.8*height) - (4.7*age)
+        }
+        NSLog(@"result: %f", result);
+        
+        //outputing the result
+        NSString *resultText = [NSString stringWithFormat:@"%f", result];
+        self.resultTextField.text = resultText;
+    
+    
 }
 @end
