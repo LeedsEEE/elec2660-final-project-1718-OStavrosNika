ad39703aca7f7881305b8775e180bbb8c5c36759
William Mooney [el16wtm] <el16wtm@eentpc244.leeds.ac.uk>
2017-12-07 19:05:34 +0000

    fixed "invalid operands to binary expression double and double"

--- Stength App/Wilks.m
+++ Stength App/Wilks.m
@@ -22,7 +22,7 @@
     NSLog(@"x: %f lift: %f", x, lift); //prints weight and lift
     
     // equation for Wilks
-    float wilks = 500/ (-126.0475144)+(16.2606339*x)+(-0.002388645*x^2)+(-0.00113732*x^3)+(7.01863E-06*x^4)+(-1.291E-08*x^5)
+    float wilks = 500/ (-126.0475144)+(16.2606339*x)+(-0.002388645*pow(x,2))+(-0.00113732*pow(x,3))+(7.01863E-06*pow(x,4))+(-1.291E-08*pow(x,5));
     NSLog(@"wilks: %f", wilks);
     
     //outputing the result
